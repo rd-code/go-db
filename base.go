413bef63e6194a0e3dc63a6795f4fdf5b3633810
黎军保 <rd@172-11-30-166.lightspeed.snantx.sbcglobal.net>
2018-04-13 11:39:07 +0800

    add function

--- base.go
+++ base.go
@@ -25,6 +25,7 @@ func queryBase(sqlStr string, args ...interface{}) (rows *sql.Rows, err error) {
     return
 }
 
+//从数据库查询字符串，单列
 func QueryString(sqlStr string, args ...interface{}) (res []string, err error) {
     var rows *sql.Rows
     if rows, err = queryBase(sqlStr, args...); err != nil {
@@ -51,5 +52,63 @@ func QueryString(sqlStr string, args ...interface{}) (res []string, err error) {
         }
     }
 
+    return
+}
+
+//从数据库查询整数信息
+func QueryInt(sqlStr string, args ...interface{}) (res []int64, err error) {
+    var rows *sql.Rows
+    if rows, err = queryBase(sqlStr, args...); err != nil {
+        return
+    }
+    defer rows.Close()
+
+    var columns []string
+    if columns, err = rows.Columns(); err != nil {
+        return
+    }
+    if len(columns) != 1 {
+        err = invalidColumnNumErr
+        return
+    }
+
+    for rows.Next() {
+        var t = &sql.NullInt64{}
+        if err = rows.Scan(t); err != nil {
+            return
+        }
+        if t.Valid {
+            res = append(res, t.Int64)
+        }
+    }
+    return
+}
+
+//从数据库查询浮点数信息
+func QueryFloat(sqlStr string, args ...interface{}) (res []float64, err error) {
+    var rows *sql.Rows
+    if rows, err = queryBase(sqlStr, args...); err != nil {
+        return
+    }
+    defer rows.Close()
+
+    var columns []string
+    if columns, err = rows.Columns(); err != nil {
+        return
+    }
+    if len(columns) != 1 {
+        err = invalidColumnNumErr
+        return
+    }
+
+    for rows.Next() {
+        var t = &sql.NullFloat64{}
+        if err = rows.Scan(t); err != nil {
+            return
+        }
+        if t.Valid {
+            res = append(res, t.Float64)
+        }
+    }
     return
 }
